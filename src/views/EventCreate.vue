36863a633b4f8e94c6baf2bd24f540ce4b21d817
StefanGhiban <stefanghiban962@gmail.com>
2022-02-23 15:38:23 +0200

    lesson

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -56,13 +56,12 @@
 
     <button type="submit">Submit</button>
   </form>
-
 </div>
 </template>
 
 <script>
 import { v4 as uuidv4 } from 'uuid'
-
+import EventService from "@/services/EventService"
 export default {
   data () {
     return {
@@ -89,9 +88,18 @@ export default {
   },
   methods: {
     onSubmit() {
-      this.event.id = uuidv4()
-      this.event.organizer = this.$store.state.user
-      console.log("Event:", this.event)
+      const event = {
+        ...this.event,
+        id: uuidv4(),
+        organizer: this.$store.state.user
+      }
+      EventService.postEvent(event)
+        .then(() => {
+          this.$store.commit('ADD_EVENT', event)
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   }
 }
